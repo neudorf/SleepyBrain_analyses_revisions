8ab53c02ab78bb9b8e85825e6dfd19f8c2ff1788
neudorf <12356878+neudorf@users.noreply.github.com>
2024-11-04 11:14:51 -0600

    r package install script added

---
+++ install_packages.r
@@ -0,0 +1,14 @@
+options(repos = c(
+    ggseg = 'https://ggseg.r-universe.dev',
+    CRAN = 'https://cloud.r-project.org'))
+
+install.packages('ggplot2')
+install.packages('wesanderson')
+install.packages('readxl')
+install.packages('tidyverse')
+install.packages('dplyr')
+install.packages('ggseg')
+install.packages('ggsegSchaefer')
+install.packages('here')
+install.packages('car')
+install.packages('effects')
