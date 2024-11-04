64ae76f822f3081a136bdfbb7dea3722fa665c2d
neudorf <12356878+neudorf@users.noreply.github.com>
2024-11-04 11:40:47 -0600

    path fix

--- 1_submit_preprocessing.sh
+++ 1_submit_preprocessing.sh
@@ -8,6 +8,7 @@
 module load StdEnv/2020 r/4.3.1 matlab/2022b.2
 source neudorf_venv/bin/activate
 export R_LIBS=~/.local/R/$EBVERSIONR/
+cd data/data_processing
 echo "Starting import_SleepyBrain_data.py"
 python -u import_SleepyBrain_data.py
 echo "done"
