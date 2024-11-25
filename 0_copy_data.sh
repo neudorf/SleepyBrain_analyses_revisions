2260b25ecbf07103c7fef5d6157490d8a78dca1c
neudorf <12356878+neudorf@users.noreply.github.com>
2024-11-25 16:05:30 -0600

    cleanup

---
+++ 0_copy_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=rrg-rmcintos
+#SBATCH --time=0-23:00:00            # Time limit (hh:mm:ss)
+#SBATCH --ntasks=1                # Number of CPU cores
+#SBATCH --mem=4G                 # Memory
+#SBATCH --nodes=1                 # Number of nodes
+
+echo starting
+cd data
+cp ~/projects/def-rmcintos/jneudorf/SleepyBrain_DMP/pipeline_outputs/connectivity_220.tar.gz .
+cp ~/projects/def-rmcintos/jneudorf/SleepyBrain_DMP/pipeline_outputs/QC_220.tar.gz .
+cp ~/projects/def-rmcintos/jneudorf/SleepyBrain_DMP/pipeline_outputs/rfMRI_niftis.tar.gz .
+tar -xf connectivity_220.tar.gz
+tar -xf QC_220.tar.gz
+tar -xf rfMRI_niftis.tar.gz
+echo done
