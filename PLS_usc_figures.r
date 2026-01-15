50114ac373d3e24e2ef4a1a1b974aa68a334e7d3
neudorf <12356878+neudorf@users.noreply.github.com>
2026-01-15 16:41:20 -0600

    figure adjustments

--- PLS_usc_figures.r
+++ PLS_usc_figures.r
@@ -5,8 +5,10 @@ library(tidyverse)
 library(here)
 
 #FC
-FC_usc_file = here('outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_usc_table.csv')
-FC_usc_plot_file = here('outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_usc_ggplot.png')
+#FC_usc_file = here('outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_usc_table.csv')
+#FC_usc_plot_file = here('outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_usc_ggplot.png')
+FC_usc_file = '/media/WDBlue/mcintosh/projects/SleepyBrain/rsfMRI_sleep_deprivation_FAIR_revisions/SleepyBrain_analyses_revisions/outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_usc_table.csv'
+FC_usc_plot_file = '/media/WDBlue/mcintosh/projects/SleepyBrain/rsfMRI_sleep_deprivation_FAIR_revisions/SleepyBrain_analyses_revisions/outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_usc_ggplot.png'
 FC_usc_df = read.csv(FC_usc_file,sep=',')
 FC_usc_df$sub = as.factor(FC_usc_df$sub)
 FC_usc_df$age = as.factor(FC_usc_df$age)
@@ -16,14 +18,15 @@ FC_usc_df[FC_usc_df$cond == 'young deprived','cond'] = 'Young (Restricted)'
 FC_usc_df[FC_usc_df$cond == 'young normal','cond'] = 'Young (Normal)'
 FC_usc_df[FC_usc_df$cond == 'old deprived','cond'] = 'Old (Restricted)'
 FC_usc_df[FC_usc_df$cond == 'old normal','cond'] = 'Old (Normal)'
-FC_usc_df$cond = factor(FC_usc_df$cond, levels=c('Young (Restricted)','Young (Normal)','Old (Restricted)', 'Old (Normal)'))
+FC_usc_df$cond = factor(FC_usc_df$cond, levels=c('Young (Normal)','Young (Restricted)','Old (Normal)', 'Old (Restricted)'))
 FC_usc_df$usc2 = as.numeric(FC_usc_df$usc2)
 FC_usc_df$orig_usc = as.numeric(FC_usc_df$orig_usc)
 FC_usc_df$ulusc = as.numeric(FC_usc_df$ulusc)
 FC_usc_df$llusc = as.numeric(FC_usc_df$llusc)
 
 #just getting this for sex variable
-sex_df = read.csv(here('data/participants.tsv'),sep='\t')
+#sex_df = read.csv(here('data/participants.tsv'),sep='\t')
+sex_df = read.csv('/media/WDBlue/mcintosh/projects/SleepyBrain/rsfMRI_sleep_deprivation_FAIR_revisions/SleepyBrain_analyses_revisions/data/participants.tsv',sep='\t')
 sex_df$sub = factor(substr(sex_df$participant_id, 5,8))
 sex_df$sex = factor(sex_df$Sex, levels=c("Male","Female"))
 sex_df = sex_df[,c('sub','sex')]
@@ -37,7 +40,7 @@ FC_usc_df_means[FC_usc_df_means$cond == 'young deprived','cond'] = 'Young (Restr
 FC_usc_df_means[FC_usc_df_means$cond == 'young normal','cond'] = 'Young (Normal)'
 FC_usc_df_means[FC_usc_df_means$cond == 'old deprived','cond'] = 'Old (Restricted)'
 FC_usc_df_means[FC_usc_df_means$cond == 'old normal','cond'] = 'Old (Normal)'
-FC_usc_df_means$cond = factor(FC_usc_df_means$cond, levels=c('Young (Restricted)','Young (Normal)','Old (Restricted)', 'Old (Normal)'))
+FC_usc_df_means$cond = factor(FC_usc_df_means$cond, levels=c('Young (Normal)','Young (Restricted)','Old (Normal)', 'Old (Restricted)'))
 
 FC_usc_df_ci = aggregate(usc2~age+sleep,x=FC_usc_df,FUN=sd)
 ggplot(FC_usc_df_means,aes(x=cond,orig_usc)) + 
@@ -53,8 +56,10 @@ ggplot(FC_usc_df_means,aes(x=cond,orig_usc)) +
 ggsave(FC_usc_plot_file)
 
 #FC Degree
-FC_usc_file = here('outputs/PLS/mean_centred_PLS/mean_centred_PLS_lv1_usc_table_FC_degree.csv')
-FC_usc_plot_file = here('outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_degree_usc_ggplot.png')
+#FC_usc_file = here('outputs/PLS/mean_centred_PLS/mean_centred_PLS_lv1_usc_table_FC_degree.csv')
+#FC_usc_plot_file = here('outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_degree_usc_ggplot.png')
+FC_usc_file = '/media/WDBlue/mcintosh/projects/SleepyBrain/rsfMRI_sleep_deprivation_FAIR_revisions/SleepyBrain_analyses_revisions/outputs/PLS/mean_centred_PLS/mean_centred_PLS_lv1_usc_table_FC_degree.csv'
+FC_usc_plot_file = '/media/WDBlue/mcintosh/projects/SleepyBrain/rsfMRI_sleep_deprivation_FAIR_revisions/SleepyBrain_analyses_revisions/outputs/PLS/mean_centred_PLS/mean_centred_PLS_FC_degree_usc_ggplot.png'
 FC_usc_df = read.csv(FC_usc_file,sep=',')
 FC_usc_df$sub = as.factor(FC_usc_df$sub)
 FC_usc_df$age = as.factor(FC_usc_df$age)
@@ -64,7 +69,7 @@ FC_usc_df[FC_usc_df$cond == 'young deprived','cond'] = 'Young (Restricted)'
 FC_usc_df[FC_usc_df$cond == 'young normal','cond'] = 'Young (Normal)'
 FC_usc_df[FC_usc_df$cond == 'old deprived','cond'] = 'Old (Restricted)'
 FC_usc_df[FC_usc_df$cond == 'old normal','cond'] = 'Old (Normal)'
-FC_usc_df$cond = factor(FC_usc_df$cond, levels=c('Young (Restricted)','Young (Normal)','Old (Restricted)', 'Old (Normal)'))
+FC_usc_df$cond = factor(FC_usc_df$cond, levels=c('Young (Normal)','Young (Restricted)','Old (Normal)', 'Old (Restricted)'))
 FC_usc_df$usc2 = as.numeric(FC_usc_df$usc2)
 FC_usc_df$orig_usc = as.numeric(FC_usc_df$orig_usc)
 FC_usc_df$ulusc = as.numeric(FC_usc_df$ulusc)
@@ -79,7 +84,7 @@ FC_usc_df_means[FC_usc_df_means$cond == 'young deprived','cond'] = 'Young (Restr
 FC_usc_df_means[FC_usc_df_means$cond == 'young normal','cond'] = 'Young (Normal)'
 FC_usc_df_means[FC_usc_df_means$cond == 'old deprived','cond'] = 'Old (Restricted)'
 FC_usc_df_means[FC_usc_df_means$cond == 'old normal','cond'] = 'Old (Normal)'
-FC_usc_df_means$cond = factor(FC_usc_df_means$cond, levels=c('Young (Restricted)','Young (Normal)','Old (Restricted)', 'Old (Normal)'))
+FC_usc_df_means$cond = factor(FC_usc_df_means$cond, levels=c('Young (Normal)','Young (Restricted)','Old (Normal)', 'Old (Restricted)'))
 FC_usc_df_ci = aggregate(usc2~age+sleep,x=FC_usc_df,FUN=sd)
 ggplot(FC_usc_df_means,aes(x=cond,orig_usc)) + 
   geom_col(aes(fill=cond), linetype = 'solid', colour = 'black', linewidth = 0.25) +
@@ -103,8 +108,8 @@ FC_usc_plot_file = '/media/WDBlue/mcintosh/projects/SleepyBrain/rsfMRI_sleep_dep
 FO_mean_plot_file = '/media/WDBlue/mcintosh/projects/SleepyBrain/rsfMRI_sleep_deprivation_FAIR_revisions/SleepyBrain_analyses_revisions/outputs/PLS/mean_centred_PLS/leida_FO_global_ggplot.png'
 FC_usc_df = read.csv(FC_usc_file,sep=',')
 FC_usc_df$sub = as.factor(FC_usc_df$sub)
-FC_usc_df$age = as.factor(FC_usc_df$age)
-FC_usc_df$sleep = as.factor(FC_usc_df$sleep)
+FC_usc_df$age = factor(FC_usc_df$age, levels = c('young','old'))
+FC_usc_df$sleep = factor(FC_usc_df$sleep, levels = c('normal','deprived'))
 FC_usc_df$cond = paste(as.character(FC_usc_df$age) , as.character(FC_usc_df$sleep))
 FC_usc_df[FC_usc_df$cond == 'young deprived','cond'] = 'Young (Restricted)'
 FC_usc_df[FC_usc_df$cond == 'young normal','cond'] = 'Young (Normal)'
@@ -159,22 +164,30 @@ ggplot(FC_usc_df_means,aes(x=cond,orig_usc)) +
   theme(text = element_text(size=20))
 ggsave(FC_usc_plot_file)
 
+lm1 = lmer(FO_global ~ sleep*age + (1 |sub),data=FC_usc_df)
+summary(lm1)
+
+int_effect = effect("sleep*age", lm1)
+FC_usc_df_means = FC_usc_df_means[order(FC_usc_df_means$cond),]
+FC_usc_df_means$lme_lower = int_effect$lower
+FC_usc_df_means$lme_upper = int_effect$upper
+FC_usc_df_means$lme_fit = int_effect$fit
+
 # Global FO mean plot
 ggplot(FC_usc_df_means,aes(x=cond,FO_global)) + 
   geom_col(aes(fill=cond), linetype = 'solid', colour = 'black', linewidth = 0.25) +
-  geom_errorbar(aes(ymin=FO_global_ll,ymax=FO_global_ul),width=.1) +
+  geom_errorbar(aes(ymin=lme_lower,ymax=lme_upper),width=.1) +
+  geom_point(data=merged_df, aes(group=cond, y = FO_global, shape=sex),  colour='black',alpha=.25) +
+  geom_line(data=merged_df, aes(y = FO_global, group=sub,linetype=sex),alpha=.5) +
   scale_fill_manual(values=c('#d33682','#d33682','#2aa198','#2aa198')) +
-  ylab('Global Coherence State FO') + xlab('Group (Sleep Condition)') + guides(fill = 'none') +
+  ylab('Global Coherence State FO') + xlab('Group (Sleep Condition)') + guides(fill = 'none') + labs(linetype='Sex',shape='Sex') +
   theme_classic() +
   theme(text = element_text(size=20), panel.border = element_blank(), panel.grid = element_blank())+
-  scale_y_continuous(expand=c(0,0)) # removes gap between 0 and y axis line
+  scale_y_continuous(limits=c(0,.8),expand=c(0,0)) # removes gap between 0 and y axis line
 
 ggsave(FO_mean_plot_file)
 
-lm1 = lmer(FO_global ~ sleep*age + (1 |sub),data=FC_usc_df)
-summary(lm1)
-plot(effect("sleep:age",lm1))
-
+plot(effect("sleep*age",lm1))
 cols = c('#444444','#2AA198','#D33682')
 pdf("/media/WDBlue/mcintosh/projects/SleepyBrain/rsfMRI_sleep_deprivation_FAIR_revisions/SleepyBrain_analyses_revisions/outputs/leida-matlab/FO_global_means.pdf", height = 6, width = 6) 
 par(mar = c(4, 5, 1, 2))
